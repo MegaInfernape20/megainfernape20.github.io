3d9a4dfd216519b8142517f759f600a133bc548d
MegaInfernape20 <andersonadrielanthony@gmail.com>
2026-07-31 23:18:57 -0400

    Update main.js

--- Lab4/Part4/main.js
+++ Lab4/Part4/main.js
@@ -97,6 +97,40 @@ class Ball extends Shape {
     
       balls.push(ball);
     }
+    class EvilCircle extends Shape {
+
+      constructor(x, y) {
+        super(x, y, 20, 20);
+    
+        this.color = "white";
+        this.size = 10;
+    
+        window.addEventListener('keydown', (e) => {
+          switch(e.key) {
+            case 'a':
+              this.x -= this.velX;
+              break;
+            case 'd':
+              this.x += this.velX;
+              break;
+            case 'w':
+              this.y -= this.velY;
+              break;
+            case 's':
+              this.y += this.velY;
+              break;
+          }
+        });
+      }
+    
+      draw() {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = 3;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+      }
+    }
     function loop() {
         ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
         ctx.fillRect(0, 0, width, height);
