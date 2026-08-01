b0e18465af48b0845cb6dedf81ce4a2d17a596ae
MegaInfernape20 <andersonadrielanthony@gmail.com>
2026-07-31 22:20:10 -0400

    Update main.js

--- Lab4/Part3/main.js
+++ Lab4/Part3/main.js
@@ -34,7 +34,7 @@ class Ball {
       ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
       ctx.fill();
     }
-    
+
     update() {
         if (this.x + this.size >= width) {
           this.velX = -Math.abs(this.velX);
@@ -55,4 +55,17 @@ class Ball {
         this.x += this.velX;
         this.y += this.velY;
       }
-}
+      collisionDetect() {
+        for (const ball of balls) {
+          if (!(this === ball)) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+    
+            if (distance < this.size + ball.size) {
+              ball.color = this.color = randomRGB();
+            }
+          }
+        }
+      }
+    }
